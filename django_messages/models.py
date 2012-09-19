4a93952eb681d2c00073d7cae285bc195c70a595
Kristopher Jelinek <kristopher.jelinek@intosite.de>
2012-09-19 13:48:16 +0200

    include system_messages

--- django_messages/models.py
+++ django_messages/models.py
@@ -56,7 +56,7 @@ class Message(models.Model):
     replied_at = models.DateTimeField(_("replied at"), null=True, blank=True)
     sender_deleted_at = models.DateTimeField(_("Sender deleted at"), null=True, blank=True)
     recipient_deleted_at = models.DateTimeField(_("Recipient deleted at"), null=True, blank=True)
-    
+    system_message = models.BooleanField(_('system_message'),default=False)
     objects = MessageManager()
     
     def new(self):
