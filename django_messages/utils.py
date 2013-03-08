8a23bb620c73df0c4e3b53ff31cf6a65b3788b5e
Kristopher Jelinek <kristopher.jelinek@intosite.de>
2013-03-08 13:39:09 +0100

    nex back

--- django_messages/utils.py
+++ django_messages/utils.py
@@ -118,7 +118,7 @@ def new_message_email(sender, instance, signal,
 
                 soup = BeautifulSoup(message_body )
                 for img in soup.findAll('img'):
-                    img['src'] = 'http://'+current_domain + img['src'][0]
+                    img['src'] = current_domain + img['src'][0]
                     message_body = str(soup)
 
                 message = render_to_string(template_name, {
